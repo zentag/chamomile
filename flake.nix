2bbc6e70a84469c181c3f191f087b7459024a0e0
zentag <38821230+zentag@users.noreply.github.com>
2026-07-14 01:21:12 -0400

    initial commit based on zmk nix template

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, zmk-nix }: let
+    forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+  in {
+    packages = forAllSystems (system: rec {
+      default = firmware;
+
+      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+        name = "firmware";
+
+        src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ];
+
+        board = "nice_nano_v2";
+        shield = "lily58_%PART%";
+
+        zephyrDepsHash = "sha256-F03oJNHWmHlpFc1JHyvqX02WL+Pg6ZcNWpCaiDfJANA=";
+
+        meta = {
+          description = "ZMK firmware";
+          license = nixpkgs.lib.licenses.mit;
+          platforms = nixpkgs.lib.platforms.all;
+        };
+      };
+
+      flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+      update = zmk-nix.packages.${system}.update;
+    });
+
+    devShells = forAllSystems (system: {
+      default = zmk-nix.devShells.${system}.default;
+    });
+  };
+}
